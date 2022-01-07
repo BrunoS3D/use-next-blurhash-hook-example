aa2d7fcd69e846ad9cb2e51bb6a8c9aca205979e
Bruno Silva <bruno3dcontato@gmail.com>
2022-01-06 22:50:02 -0300

    chore: implement use-next-blurhash hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import useNextBlurhash from 'use-next-blurhash';
 
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
+  const [blurDataUrl] = useNextBlurhash(`L*Ly+PWER+j[~WNeayWXtRWXjZoK`);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,41 +23,15 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <p className={styles.description}>This is not an official starter!</p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/lorem-picsum.jpg"
+            alt="Landscape image"
+            width="720"
+            height="360"
+            placeholder="blur"
+            blurDataURL={blurDataUrl}
+          />
         </div>
       </main>
 
